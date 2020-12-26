e7826e54b21fe2a3bdbd2ce9fbbd5ed15b7ad79d
Patrick Dockhorn <patrick@apptastic.com.au>
2020-12-26 16:33:09 +1100

    added image

---
+++ forth.c
@@ -0,0 +1,8 @@
+//
+//  forth.c
+//  
+//
+//  Created by Patrick Dockhorn on 26/12/20.
+//
+
+#include "forth.h"
---
+++ forth.h
@@ -0,0 +1,13 @@
+//
+//  forth.h
+//  
+//
+//  Created by Patrick Dockhorn on 26/12/20.
+//
+
+#ifndef forth_h
+#define forth_h
+
+#include <stdio.h>
+
+#endif /* forth_h */
